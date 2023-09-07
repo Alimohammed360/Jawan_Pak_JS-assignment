c76d2822d4b8f9f56cd3f4ea89e1496223d61334
Alimohammed360 <130058501+Alimohammed360@users.noreply.github.com>
2023-09-07 14:35:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+alert("Error! Please Enter a Valid Password");
+alert("Welcome To JS Land....\nHappy Coding!");
+
+var firstName = "Jon";
+var lastName = "Doe";
+var fullName = firstName + " " + lastName
+
+alert(firstName + " " + lastName)
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+alert("My Email Is:" + " " + "alimohamedwadiwala@gmail.com")
+
+document.write("<h2>Yah! I can write Html code on JavaScript too</h2>");
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+age = 21;
+alert("Im" + " " + age + " " + "Years Old");
+var birthYear = 2002
+document.write("<h4>My Birth Year is </h4>" + " " + birthYear+"<br>");
+var num = 16;
+var brand = "XYZ STORE"
+document.write(fullName + " " + "ordered" + num + " of T-Shirts on " + brand)
+// legal valriables
+// var fullName 
+// var full_Name
+// var $fullName
+// var full123Name
+// var fullname
+// //  illegal Variables
+// var full name 
+// var full-Name 
+// var 1fullname
+// var full&name
+// var full@anme
+
+document.write("Variable names can only contain $, _ , number and Capital Letter.");
+document.write("Variables must begin with a $, _  or letter. For example $name, _name or name.");
+document.write("Variable names are case sensitive");
+document.write("Variable names should not be JS _________"+"<br><br><br>");
+
+var a = 2;
+var b = 5;
+var c = 4;
+var d = 3;
+
+console.log("Addition ",a + b);
+console.log("Subtraction ",b - c);
+console.log("Multiply ",c * d+"<br>");
+
+var e;
+
+document.write(e+"<br>")
+
+e=5;
+
+document.write(e+"<br>")
+
+e++
+var f= e+7
+document.write(e+"<br>")
+document.write(f+"<br>")
+document.write(--f+"<br>")
+document.write(f/3+"<br>")
+document.write(f%3+"<br>")
+
+var ticket = 600;
+document.write("Price of buying 5 tickets = " ,ticket*5, "<br>")
+
+var C = 45;
+var F = (C*9/5)+32;
+
+document.write(F, "<br>")
+
+C =(F-32)*5/9;
+
+document.write(C, "<br>")
